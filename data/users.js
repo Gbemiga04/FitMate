b2a1d5bdad40a1635193461481991251a5da8064
Gbemiga04 <gbemiga49@gmail.com>
2024-09-30 19:43:52 -0500

    Fixed Bugs Surrounding User Database

--- data/users.js
+++ data/users.js
@@ -1,32 +1,32 @@
 export const users = [
-    {
-        id: "1",
-        image: "profile_pics/serena williams.jfif",
-        name: "Serena Williams",
-        distance: "",
-        description: "13x Grand Slam Champion!",
-        sports:["Tennis", "Golf", "Racing"],
-        location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  {
+      id: "1",
+      image: "profile_pics/serena williams.jfif",
+      name: "Serena Williams",
+      distance: "",
+      description: "13x Grand Slam Champion!",
+      sports: ["Tennis", "Golf"],
+      location: { lat: 40.7128, lng: -74.0060 }
+  },
+  {
       id: "2",
       image: "profile_pics/lebron-james.jpg",
       name: "LeBron James",
       distance: "",
       description: "4x NBA Champion!",
-      sports: ["Basketball"],
+      sports: ["Basketball", "Football"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "3",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Usain Bolt",
       distance: "",
       description: "Fastest Man on Earth!",
-      sports: ["Track", "Soccer"],
+      sports: ["Track", "Soccer", "Cricket"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "4",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Roger Federer",
@@ -34,35 +34,35 @@ export const users = [
       description: "20x Grand Slam Champion!",
       sports: ["Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "5",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Cristiano Ronaldo",
       distance: "",
       description: "5x Ballon d'Or Winner!",
-      sports: ["Soccer"],
+      sports: ["Soccer", "Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "6",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Simone Biles",
       distance: "",
       description: "Most Decorated Gymnast!",
-      sports: ["Gymnastics"],
+      sports: ["Gymnastics", "Track"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "7",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Michael Phelps",
       distance: "",
       description: "Most Olympic Golds!",
-      sports: ["Swimming"],
+      sports: ["Swimming", "Basketball"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "8",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Naomi Osaka",
@@ -70,17 +70,17 @@ export const users = [
       description: "4x Grand Slam Champion!",
       sports: ["Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "9",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Tom Brady",
       distance: "",
       description: "7x Super Bowl Champion!",
-      sports: ["Football"],
+      sports: ["Football", "Golf"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "10",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Lionel Messi",
@@ -88,8 +88,8 @@ export const users = [
       description: "7x Ballon d'Or Winner!",
       sports: ["Soccer"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "11",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Ronda Rousey",
@@ -97,8 +97,8 @@ export const users = [
       description: "Former UFC Champion!",
       sports: ["MMA", "Judo"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "12",
       image: "profile_pics/stephen-curry.jfif",
       name: "Stephen Curry",
@@ -106,8 +106,8 @@ export const users = [
       description: "4x NBA Champion!",
       sports: ["Basketball", "Golf"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "13",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Shaun White",
@@ -115,26 +115,26 @@ export const users = [
       description: "Snowboarding Legend!",
       sports: ["Snowboarding", "Skateboarding"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "14",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Venus Williams",
       distance: "",
       description: "7x Grand Slam Champion!",
-      sports: ["Tennis"],
+      sports: ["Tennis", "Track"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "15",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Novak Djokovic",
       distance: "",
       description: "24x Grand Slam Champion!",
-      sports: ["Tennis"],
+      sports: ["Tennis", "Soccer"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "16",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Conor McGregor",
@@ -142,8 +142,8 @@ export const users = [
       description: "Former UFC Champion!",
       sports: ["MMA", "Boxing"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "17",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Aly Raisman",
@@ -151,53 +151,53 @@ export const users = [
       description: "Olympic Gold Medalist!",
       sports: ["Gymnastics"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "18",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Russell Wilson",
       distance: "",
       description: "Super Bowl Champion!",
-      sports: ["Football"],
+      sports: ["Football", "Baseball"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "19",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Kylian Mbappe",
       distance: "",
       description: "World Cup Winner!",
-      sports: ["Soccer"],
+      sports: ["Soccer", "Basketball"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "20",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Danica Patrick",
       distance: "",
       description: "Racing Pioneer!",
-      sports: ["Racing"],
+      sports: ["Racing", "Golf"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "21",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "David Beckham",
       distance: "",
       description: "Soccer Icon!",
-      sports: ["Soccer"],
+      sports: ["Soccer", "Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "22",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Maria Sharapova",
       distance: "",
       description: "5x Grand Slam Champion!",
-      sports: ["Tennis"],
+      sports: ["Tennis", "Swimming"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "23",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Andy Murray",
@@ -205,35 +205,35 @@ export const users = [
       description: "2x Grand Slam Champion!",
       sports: ["Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "24",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Sergio Garcia",
       distance: "",
       description: "Golf Masters Champion!",
-      sports: ["Golf"],
+      sports: ["Golf", "Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "25",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Lindsey Vonn",
       distance: "",
       description: "Skiing Legend!",
-      sports: ["Skiing"],
+      sports: ["Skiing", "Tennis"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "26",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Sidney Crosby",
       distance: "",
       description: "3x Stanley Cup Winner!",
-      sports: ["Hockey"],
+      sports: ["Hockey", "Golf"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "27",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Alex Morgan",
@@ -241,86 +241,32 @@ export const users = [
       description: "Soccer World Champion!",
       sports: ["Soccer"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "28",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Wayne Gretzky",
       distance: "",
       description: "Hockey GOAT!",
-      sports: ["Hockey"],
+      sports: ["Hockey", "Baseball"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "29",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Paul Pogba",
       distance: "",
       description: "World Cup Champion!",
-      sports: ["Soccer"],
+      sports: ["Soccer", "Basketball"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
+  },
+  {
       id: "30",
       image: "profile_pics/blank-profile-picture-png.webp",
       name: "Michael Jordan",
       distance: "",
       description: "6x NBA Champion!",
-      sports: ["Basketball"],
+      sports: ["Basketball", "Baseball"],
       location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
-      id: "31",
-      image: "profile_pics/blank-profile-picture-png.webp",
-      name: "Peyton Manning",
-      distance: "",
-      description: "2x Super Bowl Champion!",
-      sports: ["Football"],
-      location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
-      id: "32",
-      image: "profile_pics/blank-profile-picture-png.webp",
-      name: "Manny Pacquiao",
-      distance: "",
-      description: "Boxing Legend!",
-      sports: ["Boxing"],
-      location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
-      id: "33",
-      image: "profile_pics/blank-profile-picture-png.webp",
-      name: "Jordan Spieth",
-      distance: "",
-      description: "Golf Masters Winner!",
-      sports: ["Golf"],
-      location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
-      id: "34",
-      image: "profile_pics/blank-profile-picture-png.webp",
-      name: "Jimmy Butler",
-      distance: "",
-      description: "NBA Star!",
-      sports: ["Basketball"],
-      location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
-      id: "35",
-      image: "profile_pics/blank-profile-picture-png.webp",
-      name: "Jamie Whincup",
-      distance: "",
-      description: "Supercar Champion!",
-      sports: ["Racing"],
-      location: { lat: 40.7128, lng: -74.0060 }
-    },
-    {
-      id: "36",
-      image: "profile_pics/blank-profile-picture-png.webp",
-      name: "Sloane Stephens",
-      distance: "",
-      description: "US Open Champion!",
-      sports: ["Tennis"],
-      location: { lat: 40.7128, lng: -74.0060 }
-    }
-  ]
+  }
+];
